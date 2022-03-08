a7d7b1818dde95e61166f54c4bf6c86e967d184e
woobba94 <jwj3199@naver.com>
2022-03-08 23:14:11 +0900

    ✨ Init : Pages

---
+++ src/ProductDetailPage.js
@@ -0,0 +1,10 @@
+export default function ProductDetailPage({ $target }) {
+  const $page = document.createElement("div");
+
+  $page.className = "ProductDetailPage";
+  $page.innerHTML = "<h1>OOO 상품 정보</h1>";
+
+  this.render = () => {
+    $target.appendChild($page);
+  };
+}
